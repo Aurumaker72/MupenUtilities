a04f9b563b3489da542f03d093986ed164c16a93
Aurumaker72 <armkdev72@gmail.com>
2021-04-10 10:29:37 +0200

    Add some more tips

--- Helpers/TipProvider.cs
+++ Helpers/TipProvider.cs
@@ -10,7 +10,12 @@ namespace MupenUtils.Helpers
             "Right click the M64 radio button to bypass the file filter",
             "Press backslash to frame advance",
             "Right click on the joystick to make it follow your mouse",
-            "Pressing Escape on the X and Y textboxes will unfocus them"
+            "Pressing Escape on the X and Y textboxes will unfocus them",
+            "\'Sticky\' will make your inputs stay on the next frame",
+            "\'Dump Inputs\' will dump all input data to a file",
+            "Try pressing \'Page up\' and \'Page down\' on the trackbar",
+            "Check for updates with the button below",
+            "You are on version " + MainForm.PROGRAM_VERSION
         };
         public static string GetRandomTip()
         {
