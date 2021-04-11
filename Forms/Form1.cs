d598ba1bff148452512b38eda33316cff7e95dfb
Aurumaker72 <armkdev72@gmail.com>
2021-04-11 12:02:32 +0200

    Remove unused imports

--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
--- Helpers/UIHelper.cs
+++ Helpers/UIHelper.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.WindowsAPICodePack.Dialogs;
-using System;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace MupenUtils
