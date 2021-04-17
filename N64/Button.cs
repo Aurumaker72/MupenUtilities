b90eff04fd09c401e600366f3b5d28ad07d7bd66
Aurumaker72 <armkdev72@gmail.com>
2021-04-17 20:46:45 +0200

    Add button class (to be used later)

---
+++ N64/Button.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MupenUtils.N64
+{
+    public class Button
+    {
+        public int indexInStruct;
+
+        public bool internalPressed;
+        public bool userPressed;
+        public bool autoFire;
+
+        public Button(int indexInStruct)
+        {
+            this.indexInStruct = indexInStruct;
+        }
+    }
+}
